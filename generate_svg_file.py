234881bd0a2a07ca7aaa52ef6f86fdb88c16b022
Donovan Rinquebach <donovanrinquebach25@icloud.com>
2026-06-01 11:32:11 +0200

    Ajout du workflow GitHub Actions et du script de génération statique

---
+++ generate_svg_file.py
@@ -0,0 +1,17 @@
+from github_service import fetch_github_data
+from svg_generator import generate_svg
+
+def main():
+    # 1. Récupère les données
+    data = fetch_github_data("donovan057")
+
+    # 2. Génère le SVG
+    svg_content = generate_svg(data)
+
+    # 3. Sauvegarde dans un fichier stats.svg
+    with open("stats.svg", "w", encoding="utf-8") as f:
+        f.write(svg_content)
+    print("Fichier stats.svg généré avec succès !")
+
+if __main__ == "__name__":
+    main()
